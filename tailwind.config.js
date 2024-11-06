aa972b389ec756d0061acc8c655463e4a3fb919a
Jacob Hiney <hiney.jake@outlook.com>
2024-11-06 16:07:18 -0600

    CSS for CRT-like effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {
+      boxShadow: {
+        'glow': '0 0 10px rgba(0, 255, 0, 0.2), 0 0 20px rgba(0, 255, 0, 0.1)',
+      },
+    },
   },
-  plugins: []
-};
+  plugins: [],
+}
